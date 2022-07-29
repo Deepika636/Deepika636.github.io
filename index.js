999cdef50dd32689de1203b3607c7f87274c1374
Deepika636 <80308676+Deepika636@users.noreply.github.com>
2022-07-29 18:18:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(".number-button").click(function(){
   var rateString="You selected "+rating+" out of 5";
   $(".sub").click(function(event){
 
-     $(".main").html('<img class="secImg" src="images/illustration-thank-you.svg">\
+     $(".main").html('<img class="secImg" src="illustration-thank-you.svg">\
      <p id="rate"> </p>\
      <h2 class="sec">Thank You!</h2>\
      <p class="sec">We appreciate you taking the time to give a rating. If you ever need more support do not hesitate to get in touch!</p>');
